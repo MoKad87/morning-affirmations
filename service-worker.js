a2e93ff9a63c97debb0a4a8b2aa2731e5a6bbd6a
MoKad87 <31776127+MoKad87@users.noreply.github.com>
2026-01-29 12:25:46 -0800

    Update and rename README.md to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "gbb-commands-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
